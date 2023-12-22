ddee3509278128d97a50cff5efc89757a0676c84
Vaishnavi Datir <122637084+vaishnavidatir06@users.noreply.github.com>
2023-12-22 16:10:53 +0530

    Add files via upload

---
+++ Weather/index.js
@@ -0,0 +1,28 @@
+var inputvalue = document.querySelector('#cityinput');
+var btn = document.querySelector('#submitBtn'); // Changed from 'add' to 'submitBtn'
+var city = document.querySelector('#cityoutput');
+var description = document.querySelector('#description');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var apik = "59cd5627b75abcf8104fc903993477ac";
+
+function convertion(val) {
+  return (val - 273).toFixed(3);
+}
+
+btn.addEventListener('click', function () {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik)
+    .then(res => res.json())
+    .then(data => {
+      var nameval = data['name'];
+      var weatherDescription = data['weather'][0]['description'];
+      var temperature = data['main']['temp'];
+      var windSpeed = data['wind']['speed'];
+
+      city.innerHTML = `Weather of <span>${nameval}</span>`;
+      temp.innerHTML = `Temperature: <span>${convertion(temperature)} C</span>`;
+      description.innerHTML = `Sky Conditions: <span>${weatherDescription}</span>`;
+      wind.innerHTML = `Wind Speed: <span>${windSpeed}</span>`;
+    })
+    .catch(err => alert('You entered the wrong city name'));
+});
